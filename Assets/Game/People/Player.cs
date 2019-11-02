71b2c55bb8876050417615c0c27f54738d515d12
Jamil <jamil.jewel@jet.com>
2019-11-02 12:42:14 -0400

    add a player sprite with direction control

---
+++ Assets/Game/People/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public enum Direction { None = 0, Up, Down, Left, Right }
+
+    Direction _lastDir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Direction.None;
+
+        var y = Input.GetAxis("Vertical");
+        if (y < 0)
+        {
+            dir = Direction.Down;
+        }
+        else if (y > 0)
+        {
+            dir = Direction.Up;
+        }
+
+        var x = Input.GetAxis("Horizontal");
+        if (x < 0)
+        {
+            dir = Direction.Left;
+        }
+        else if (x > 0)
+        {
+            dir = Direction.Right;
+        }
+
+        if (dir != _lastDir)
+        {
+            _animator.SetInteger("direction", (int) dir);
+            _lastDir = dir;
+        }
+        else if(_lastDir != Direction.None)
+        {
+            //set to none so animation just repeats without transition
+            _animator.SetInteger("direction", (int) Direction.None);
+        }
+
+        const float speed = .05f;
+        
+        transform.Translate(x * speed, y * speed, 0);
+    }
+}
