0b69dca1e9f863b650423a1449e2e29a2318cd6a
Jamil <jamil.jewel@jet.com>
2019-11-04 23:41:13 -0500

    player stop start walking control

--- Assets/Game/People/Player.cs
+++ Assets/Game/People/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _animator.SetInteger("direction", (int) _lastDir);
+        _animator.SetBool("walk", false);
     }
 
     public enum Direction { None = 0, Up, Down, Left, Right }
@@ -43,23 +45,34 @@ public class Player : MonoBehaviour
             dir = Direction.Right;
         }
 
-        if (dir != _lastDir)
+        var walk = dir != Direction.None;
+
+
+        if (dir != _lastDir || walk != _animator.GetBool("walk"))
         {
-            _animator.SetInteger("direction", (int) dir);
+            if (dir == Direction.None)
+                _animator.SetInteger("direction", (int)_lastDir);
+            else
+                _animator.SetInteger("direction", (int)dir);
+
+            _animator.SetBool("walk", walk);
             _lastDir = dir;
         }
-        else if(_lastDir != Direction.None)
+        else if (_lastDir != Direction.None && walk)
         {
             //set to none so animation just repeats without transition
-            _animator.SetInteger("direction", (int) Direction.None);
+            _animator.SetInteger("direction", (int)Direction.None);
         }
 
-        const float speed = .05f;
-        
-        transform.Translate(x * speed, y * speed, 0);
+        if(walk)
+        {
+            const float speed = .05f;
 
-        var camX = Mathf.Clamp(transform.position.x, -5, 4);
-        var camY = Mathf.Clamp(transform.position.y, 0, 14);
-        Camera.transform.SetPositionAndRotation(new Vector3(camX, camY, Camera.transform.position.z), Camera.transform.rotation);
+            transform.Translate(x * speed, y * speed, 0);
+
+            var camX = Mathf.Clamp(transform.position.x, -5, 4);
+            var camY = Mathf.Clamp(transform.position.y, 0, 14);
+            Camera.transform.SetPositionAndRotation(new Vector3(camX, camY, Camera.transform.position.z), Camera.transform.rotation);
+        }
     }
 }
